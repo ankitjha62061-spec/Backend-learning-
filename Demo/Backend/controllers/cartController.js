0601431183339ec9866ee7c1ee2c903af4bdea8b
Ankit Jha <ankitjha62061@gmail.com>
2026-05-27 11:28:35 +0530

    Implemented add and delete cart functionality with Postman testing

--- Demo/Backend/controllers/cartController.js
+++ Demo/Backend/controllers/cartController.js
@@ -2,30 +2,40 @@ const User = require("../models/User");
 
 exports.addToCart = async (req, res) => {
   try {
-    const userId = req.user.id;
-    const productId = req.body.productId;
+    const user = await User.findById(req.user.id);
 
-    const user = await User.findById(userId);
-
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-  
     user.cart.push({
-      productId: productId,
+      productId: req.body.productId,
       quantity: 1
     });
 
     await user.save();
 
     res.json({
-      message: "Product added to cart",
+      message: "Added to cart",
       cart: user.cart
     });
 
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 };
+exports.deleteFromCart = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
 
+    user.cart = user.cart.filter(
+      item => item._id != req.params.id
+    );
+
+    await user.save();
+
+    res.json({
+      message: "Deleted from cart",
+      cart: user.cart
+    });
+
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
--- Demo/Backend/routes/cartRoutes.js
+++ Demo/Backend/routes/cartRoutes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const authMiddleware = require("../middleware/authMiddleware");
-const { addToCart } = require("../controllers/cartController");
 
+const {
+  addToCart,
+  deleteFromCart
+} = require("../controllers/cartController");
 
 router.post("/add", authMiddleware, addToCart);
+router.delete("/delete/:id", authMiddleware, deleteFromCart);
 
 module.exports = router;
-
- 
