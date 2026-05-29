03404126230391c3b772ab4c8c5770d13f2c5095
Ankit Jha <ankitjha62061@gmail.com>
2026-05-29 19:01:24 +0530

    chnages

--- Demo/Backend/routes/userRouter.js
+++ Demo/Backend/routes/userRouter.js
@@ -5,27 +5,32 @@ const { createToken } = require("../utils/jwt");
 const authMiddleware = require("../middleware/authMiddleware");
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 const upload = require("../middleware/upload");
+const SECRET = "mysecretkey";
+
+// signup in mongo
 
 router.post("/signup", async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
+ try {
 
-    if (!name || !email || !password) {
-      return res.status(400).json({
+    const { name, email, password } = req.body;
+   if (!name || !email || !password) {
+     return res.status(400).json({
         message: "Please enter name, email and password",
       });
-    }
 
+    }
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
+
       return res.status(400).json({
         message: "Email already registered",
       });
-    }
 
-    const hash = await bcrypt.hash(password, 10);
+    }
+    const hash = bcrypt.hashSync(password, 10);
 
     const newUser = new User({
       name,
@@ -36,257 +41,410 @@ router.post("/signup", async (req, res) => {
     await newUser.save();
 
     res.status(201).json({
-      message: "Signup successful",
+      message: "Signup successful (saved in MongoDB)",
     });
+
   } catch (error) {
+
     res.status(500).json({
-      message: error.message,
+      message: "Error",
+      error: error.message,
     });
+
   }
+
 });
 
+
+// login mongo
+
 router.post("/login", async (req, res) => {
+
   try {
+
     const { email, password } = req.body;
 
     if (!email || !password) {
+
       return res.status(400).json({
-        message: "Email or password is required",
+        message: "Email or password is required"
       });
+
     }
 
     const user = await User.findOne({ email });
 
     if (!user) {
+
       return res.status(404).json({
-        message: "User not found",
+        message: "User not found"
       });
+
     }
 
-    const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch =
+      bcrypt.compareSync(password, user.password);
 
     if (!isMatch) {
+
       return res.status(400).json({
-        message: "Wrong password",
+        message: "Wrong password"
       });
+
     }
 
     const token = createToken(user);
 
-    res.status(200).json({
+    return res.status(200).json({
+
       message: "Login successful",
+
       token,
+
       user: {
         name: user.name,
         email: user.email,
       },
+
     });
+
   } catch (error) {
-    res.status(500).json({
-      message: error.message,
+
+    return res.status(500).json({
+
+      message: "Server error",
+
+      error: error.message,
+
     });
+
   }
+
 });
 
+
+
 router.get("/", authMiddleware, async (req, res) => {
+
   try {
+
     const search = req.query.search || "";
     const page = Number(req.query.page) || 1;
+
     const limit = 3;
 
     const skip = (page - 1) * limit;
 
     const users = await User.find({
-      name: { $regex: search, $options: "i" },
+
+      name: {
+        $regex: search,
+        $options: "i",
+      },
+
     })
       .select("-password")
       .skip(skip)
       .limit(limit);
 
-    const totalUsers = await User.countDocuments({
-      name: { $regex: search, $options: "i" },
-    });
+    const totalUsers =
+      await User.countDocuments({
+
+        name: {
+          $regex: search,
+          $options: "i",
+        },
+
+      });
+
 
     res.json({
+
       users,
+
       currentPage: page,
-      totalPages: Math.ceil(totalUsers / limit),
+
+      totalPages:
+        Math.ceil(totalUsers / limit),
+
     });
+
   } catch (error) {
-    res.status(500).json({ message: error.message });
+
+    res.status(500).json({
+
+      message: error.message,
+
+    });
+
   }
+
 });
 
-// ---------------------- CREATE USER ----------------------
 router.post("/", authMiddleware, async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
 
-    const existingUser = await User.findOne({ email });
+  const { name, email, password } = req.body;
 
-    if (existingUser) {
-      return res.status(400).json({
-        message: "Email already exists",
-      });
-    }
+  const hash = bcrypt.hashSync(password, 10);
 
-    const hash = await bcrypt.hash(password, 10);
+  const newUser = new User({
 
-    const newUser = new User({
-      name,
-      email,
-      password: hash,
-    });
+    name,
 
-    await newUser.save();
+    email,
 
-    res.json({
-      message: "User created",
-      user: newUser,
-    });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+    password: hash,
+
+  });
+
+  await newUser.save();
+
+  res.json({
+
+    message: "User created",
+
+    user: newUser,
+
+  });
+
 });
 
 router.put("/:id", authMiddleware, async (req, res) => {
+
   try {
+
     const { name, email } = req.body;
+
     const userId = req.params.id;
 
-    if (email) {
-      const existingEmail = await User.findOne({
-        email,
-        _id: { $ne: userId },
+    const existingEmail = await User.findOne({
+
+      email,
+
+      _id: { $ne: userId },
+
+    });
+
+
+
+
+    if (existingEmail) {
+
+      return res.status(400).json({
+
+        message: "Email already exists",
+
       });
 
-      if (existingEmail) {
-        return res.status(400).json({
-          message: "Email already exists",
-        });
-      }
     }
 
-    const updatedUser = await User.findByIdAndUpdate(
-      userId,
-      { name, email },
-      { new: true }
-    ).select("-password");
+    const updatedUser =
+      await User.findByIdAndUpdate(
+
+        userId,
+
+        {
+          name,
+          email,
+        },
+
+        { new: true }
+
+      ).select("-password");
 
     if (!updatedUser) {
+
       return res.status(404).json({
+
         message: "User not found",
+
       });
+
     }
 
     res.json({
+
       message: "User updated",
+
       user: updatedUser,
+
     });
+
   } catch (error) {
-    res.status(500).json({ message: error.message });
+
+    res.status(500).json({
+
+      message: error.message,
+
+    });
+
   }
+
 });
 
+
+
+// patch
+
 router.patch("/:id", authMiddleware, async (req, res) => {
-  try {
-    const userId = req.params.id;
 
-    // email uniqueness check
-    if (req.body.email) {
-      const existingEmail = await User.findOne({
-        email: req.body.email,
-        _id: { $ne: userId },
-      });
+  const updatedUser =
+    await User.findByIdAndUpdate(
 
-      if (existingEmail) {
-        return res.status(400).json({
-          message: "Email already exists",
-        });
-      }
-    }
+      req.params.id,
 
-    const updatedUser = await User.findByIdAndUpdate(
-      userId,
       req.body,
-      { new: true }
+
+      { returnDocument: "after" }
+
     ).select("-password");
 
-    if (!updatedUser) {
-      return res.status(404).json({
-        message: "User not found",
-      });
-    }
 
-    res.json({
-      message: "User patched",
-      user: updatedUser,
+
+  if (!updatedUser) {
+
+    return res.json({
+
+      message: "User not found",
+
     });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+
   }
+
+  res.json({
+
+    message: "User patched",
+
+    user: updatedUser,
+
+  });
+
 });
 
 router.delete("/:id", authMiddleware, async (req, res) => {
-  try {
-    const deletedUser = await User.findByIdAndDelete(req.params.id);
 
-    if (!deletedUser) {
-      return res.status(404).json({
-        message: "User not found",
-      });
-    }
+  const deletedUser =
+    await User.findByIdAndDelete(req.params.id);
 
-    res.json({
-      message: "User deleted",
+  if (!deletedUser) {
+
+    return res.json({
+
+      message: "User not found",
+
     });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+
   }
+
+  res.json({
+
+    message: "User deleted",
+
+  });
+
 });
 
-// ---------------------- PROFILE GET ----------------------
-router.get("/profile", authMiddleware, async (req, res) => {
-  try {
-    const user = await User.findById(req.user.id).select("-password");
 
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+//  get log in profile  that shows in profile systemmm  after login 
 
-router.put(
+router.get(
+
   "/profile",
+
   authMiddleware,
-  upload.single("profileImage"),
+
   async (req, res) => {
+
     try {
-      const { name, mobile, address } = req.body;
 
-      const updatedData = {};
+      const user =
+        await User.findById(req.user.id)
+        .select("-password");
 
-      if (name) updatedData.name = name;
-      if (mobile) updatedData.mobile = mobile;
-      if (address) updatedData.address = address;
-      if (req.file) updatedData.profileImage = req.file.filename;
+      res.status(200).json(user);
 
-      const updatedUser = await User.findByIdAndUpdate(
-        req.user.id,
-        updatedData,
-        { new: true }
-      ).select("-password");
-
-      res.status(200).json({
-        message: "Profile Updated",
-        user: updatedUser,
-      });
     } catch (error) {
+
       res.status(500).json({
         message: error.message,
       });
+
     }
+
   }
+
 );
+
+//  for profile    update that i make in for profile system 
+router.put(
+
+  "/profile",
+
+  authMiddleware,
+
+  upload.single("profileImage"),
+
+  async (req, res) => {
+
+    try {
+
+      const {
+        name,
+        email,
+        mobile,
+        address,
+      } = req.body;
+
+      const updatedData = { name,email, mobile,address, };
+
+
+    
+      
+      if (req.file) {
+        updatedData.profileImage =
+          req.file.filename;
+
+      }
+
+      const updatedUser =
+        await User.findByIdAndUpdate(
+
+          req.user.id,
+
+          updatedData,
+
+          { new: true }
+
+        ).select("-password");
+
+
+
+
+      res.status(200).json({
+
+        message: "Profile Updated",
+
+        user: updatedUser,
+
+      });
+
+    } catch (error) {
+
+      res.status(500).json({
+
+        message: error.message,
+
+      });
+
+    }
+
+  }
+
+);
+
+
+
+
+
 
 module.exports = router;
